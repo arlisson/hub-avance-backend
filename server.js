52114a1ffc37526f2a9f7bba5a15194605254a50
Árlisson Alves <arlisson234529@gmail.com>
2026-03-19 14:32:32 -0300

    Teste de rota 2

--- server.js
+++ server.js
@@ -1,4 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import path from "path";
+import { fileURLToPath } from "url";
 import { pool } from "./src/config/db.js";
+
+dotenv.config();
+
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/api/health", async (req, res) => {
   try {
@@ -7,4 +25,8 @@ app.get("/api/health", async (req, res) => {
   } catch (error) {
     res.status(500).json({ ok: false, error: error.message });
   }
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Servidor rodando na porta ${process.env.PORT || 3000}`);
 });
